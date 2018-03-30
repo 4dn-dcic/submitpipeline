3c9e8525b13380734761b1ece0e25daacb7da2e8
SooLee <duplexa@gmail.com>
2018-03-30 11:08:53 -0400

    .

---
+++ submitpipeline/repliseq.py
@@ -0,0 +1,7 @@
+from submitpipeline.cwl2workflow import add_workflow_to_insert, add_software_to_insert
+basedir = "/Users/soo/git/fourfront/src/encoded/tests/data/inserts"
+docker_version = 'v12'
+
+add_software_to_insert('4dn-dcic/docker-4dn-repliseq', docker_version, basedir + '/software.json')
+add_workflow_to_insert(basedir + '/workflow.json', basedir + '/software.json', 'repliseq-parta.cwl', 'repliseq', 'dev')
+
